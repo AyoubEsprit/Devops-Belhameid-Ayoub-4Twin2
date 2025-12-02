5faee6d5b0df12de0b1102bfd7401bfd8adb7813
Ayoub belhameid <Ayoub.Belhameid@esprit.tn>
2025-12-02 09:55:45 +0100

    Tests

--- src/test/java/tn/esprit/studentmanagement/Services/DepartmentServiceTest.java
+++ src/test/java/tn/esprit/studentmanagement/Services/DepartmentServiceTest.java
@@ -1,4 +1,4 @@
-package tn.esprit.studentmanagement;
+package tn.esprit.studentmanagement.Services;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
---
+++ src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTest.java
@@ -0,0 +1,16 @@
+package tn.esprit.studentmanagement;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Disabled
+
+class StudentManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
